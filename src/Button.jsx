31a82f47f0afcdc7cb9da721d0ce79776f0ae039
cbndev99 <chandev99@hotmail.com>
2024-02-18 21:13:34 +0800

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -10,6 +10,20 @@ function Button() {
     marginBottom: "10px",
   }
 
-  return <button style={styles}>CLICK ME</button>
+  let count = 0
+  const handleClick = (name) => {
+    if (count < 3) {
+      count++
+      console.log(`${name} you clicked me ${count} times.`)
+    } else {
+      console.log(`${name} stop clicking me.`)
+    }
+  }
+
+  return (
+    <button style={styles} onClick={() => handleClick("Bro")}>
+      CLICK ME
+    </button>
+  )
 }
 export default Button
